0cef0ba0fe68dd759c8dbfb3fcf0329b6b2c8ec6
David Chen <david.chen@earthscope.org>
2025-02-11 17:00:50 -0800

    finish implementation for problem 2

--- 2/main.go
+++ 2/main.go
@@ -93,17 +93,31 @@ Just use a sorted array, append, and try sort.searchInts to get something initia
 research performance after.
 */
 func insert(store *[]InsertMessage, timestamp int32, price int32) {
+	//fmt.Printf("Inserting %d %d", timestamp, price)
 	msg := InsertMessage{timestamp, price}
 	*store = append(*store, msg)
 }
 
-func query(store []InsertMessage, minTs int32, maxTs int32) float32 {
+func query(store []InsertMessage, getter func(x InsertMessage) int, minTs int32, maxTs int32) float64 {
+	//fmt.Printf("Querying %d %d\n", minTs, maxTs)
 	// Find the average price
+	if len(store) == 0 {
+		return 0.0
+	}
 	//getter := func(x InsertMessage) int { return int(x.Timestamp) }
-	//minIndex := findMinIndex(store, int(minTs), getter)
-	//maxIndex := findMaxIndex(store, int(maxTs), getter)
-
-	return 0.0
+	minIndex := findMinIndex(store, int(minTs), getter)
+	maxIndex := findMaxIndex(store, int(maxTs), getter)
+	var total int32 = 0
+	var count int32 = 0
+	for i := minIndex; i <= maxIndex; i++ {
+		// Need this duplicate condition check because findIndex gets closest numbers,
+		// allowing for flexible queries.
+		if store[i].Timestamp >= minTs && store[i].Timestamp <= maxTs {
+			total += store[i].Price
+			count += 1
+		}
+	}
+	return float64(total) / float64(count)
 }
 
 func handleConnection(conn net.Conn) {
@@ -114,6 +128,7 @@ func handleConnection(conn net.Conn) {
 
 	buf := make([]byte, 9)
 	store := make([]InsertMessage, 0)
+	getter := func(x InsertMessage) int { return int(x.Timestamp) }
 	for {
 		n, err := conn.Read(buf)
 		if err != nil {
@@ -132,15 +147,16 @@ func handleConnection(conn net.Conn) {
 			insert(&store, msg.Timestamp, msg.Price)
 		case 'Q':
 			var msg QueryMessage
-			var avg float32
+			var avg float64
 			err = binary.Read(bytes.NewBuffer(buf[1:]), binary.BigEndian, &msg)
-			avg = query(store, msg.MinTimestamp, msg.MaxTimestamp)
+			avg = query(store, getter, msg.MinTimestamp, msg.MaxTimestamp)
 			var output string
-			if math.Trunc(float64(avg)) == float64(avg) {
+			if math.Trunc(avg) == avg {
 				output = strconv.FormatInt(int64(avg), 10)
 			} else {
-				output = strconv.FormatFloat(float64(avg), 'f', -1, 32)
+				output = strconv.FormatFloat(avg, 'f', -1, 32)
 			}
+			fmt.Printf("Sending %s\n", output)
 			_, err = conn.Write([]byte(output))
 			if err != nil {
 				fmt.Println("Error writing:", err)
--- 2/main_test.go
+++ 2/main_test.go
@@ -7,16 +7,15 @@ import (
 
 func TestProcess(t *testing.T) {
 	store := make([]InsertMessage, 0)
-	assert.Equal(t, 0, query(store, 0, 20))
+	assert.Equal(t, 0.0, query(store, 0, 20))
 	insert(&store, 0, 100)
-	assert.Equal(t, 100, query(store, 0, 20))
+	assert.Equal(t, 100.0, query(store, 0, 20))
 	insert(&store, 5, 20)
-	assert.Equal(t, 60, query(store, 0, 20))
-	assert.Equal(t, 0, query(store, 10, 20))
+	assert.Equal(t, 60.0, query(store, 0, 20))
+	assert.Equal(t, 0.0, query(store, 10, 20))
 }
 
 func TestFindMinMax(t *testing.T) {
-	// TODO: Fix the near min/max search
 	arr := []int{1, 5, 10, 18, 21}
 	assert.Equal(t, 1, findMinInt(arr, 0))
 	assert.Equal(t, 1, findMinInt(arr, 1))
