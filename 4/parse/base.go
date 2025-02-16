17e38f4b9d5302f8556b8585110ad9f3d2c9c0d2
David Chen <david.chen@earthscope.org>
2025-02-15 21:42:14 -0800

    switch to strings.Cut

--- 4/parse/base.go
+++ 4/parse/base.go
@@ -5,10 +5,6 @@ import "strings"
 func ParseMessage(msg string) (key string, value string, isInsert bool) {
 	// Returns key, value if its insert
 	// Returns key, value="" if its retrieve
-	result := strings.SplitN(msg, "=", 2)
-	if len(result) == 1 {
-		return result[0], "", false
-	} else {
-		return result[0], result[1], true
-	}
+	before, after, splitExists := strings.Cut(msg, "=")
+	return before, after, splitExists
 }
