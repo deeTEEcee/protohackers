23358ab1221c2412d48ffcc7fa7fe77404e0c218
David Chen <david.chen@earthscope.org>
2025-02-19 09:37:13 -0800

    add proj initial project 5 setup

---
+++ 5/main.go
@@ -0,0 +1,77 @@
+package main
+
+import (
+	"fmt"
+	"log"
+	"net"
+	"os"
+	. "protohackers/tcp"
+)
+
+func handleClient(client net.Conn, upstream net.Conn) {
+	defer func() {
+		log.Println("Closing connection")
+		client.Close()
+	}()
+	// 1. Initialize the chatroom name first
+	initServerMsg := ReadMessage(upstream)
+	WriteMessage(client, initServerMsg)
+
+	initClientResponse := ReadMessage(client)
+	WriteMessage(upstream, initClientResponse)
+
+	// TODO: Remove this stub later but why are we not receiving 'The room contains'
+	serverMsg := ReadMessage(upstream)
+	WriteMessage(client, serverMsg)
+
+	// 2. After step 1, we just have to send every upstream message back down to the client
+	go handleUpstream(client, upstream)
+
+	for {
+		// 3. Client can handle sending messages upstream here
+		success := runOnce(client, upstream)
+		if !success {
+			return
+		}
+	}
+}
+
+func handleUpstream(client net.Conn, upstream net.Conn) {
+	// We don't know when to expect upstream messages. So this has to be handled
+	// separately from client messages
+	message := ReadMessage(upstream)
+	// TODO: Filter message here and replace bitcoin
+	WriteMessage(client, message)
+}
+
+func runOnce(client net.Conn, upstream net.Conn) bool {
+	message := ReadMessage(client)
+	// TODO: Filter message here and replace bitcoin
+	WriteMessage(upstream, message)
+	return true
+}
+
+func startServer(address string) {
+	listener, err := net.Listen("tcp", address)
+	if err != nil {
+		fmt.Println("Error listening:", err)
+		os.Exit(1)
+	}
+	defer listener.Close()
+
+	fmt.Println("Server listening on :8080")
+
+	for {
+		client, err := listener.Accept()
+		if err != nil {
+			fmt.Println("Error accepting:", err)
+			continue
+		}
+		upstream := SetupUpstream("chat.protohackers.com:16963")
+		go handleClient(client, upstream)
+	}
+}
+
+func main() {
+	startServer(":8080")
+}
---
+++ 5/tcp/base.go
@@ -0,0 +1,40 @@
+package tcp
+
+import (
+	"bufio"
+	"log"
+	"net"
+	"strings"
+	"unicode"
+)
+
+func SetupUpstream(addr string) net.Conn {
+	conn, err := net.Dial("tcp", addr)
+	if err != nil {
+		log.Printf("Dial failed: %s\n", err.Error())
+	}
+	return conn
+}
+
+func ReadMessage(conn net.Conn) string {
+	// We can use this for reading messages from any connection
+	reader := bufio.NewReader(conn)
+	message, err := reader.ReadString('\n')
+	if err != nil {
+		// This can occur with EOF when the client disconnects intentionally or not.
+		log.Printf("Error occurred while reading from client: %s\n", err)
+		return ""
+	}
+	message = strings.TrimRightFunc(message, unicode.IsSpace)
+	log.Printf("Read message '%s' from (%s)\n", message, conn.LocalAddr())
+	return message
+}
+
+func WriteMessage(conn net.Conn, message string) {
+	_, err := conn.Write([]byte(message))
+	if err != nil {
+		log.Printf("Error occurred while sending: %s\n", err)
+		return
+	}
+	log.Printf("Writing '%s' to (%s)\n", message, conn.LocalAddr())
+}
